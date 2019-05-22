3c226601e122f14fc2a44ca2a2b84b52ddcc26e7
Jan Biasi <biasijan@gmail.com>
2019-05-23 01:06:50 +0200

    try lambda

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import app, { handler } from './app';
+import app, { handler as serverlessApplicationHandler } from './app';
 import { PRODUCTION } from './config';
 
-const port = PRODUCTION ? 80 : 9003;
+// const port = PRODUCTION ? 80 : 9003;
 
-// Start server
-app.listen(port, () => {
-    console.log(
-        'Server listening on port %s in %s mode ...',
-        port,
-        PRODUCTION ? 'production' : 'development'
-    );
-});
+// // Start server
+// app.listen(port, () => {
+//     console.log(
+//         'Server listening on port %s in %s mode ...',
+//         port,
+//         PRODUCTION ? 'production' : 'development'
+//     );
+// });
+
+export const handler = serverlessApplicationHandler;
